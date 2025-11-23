e727baea7ba2f13d2ea72aa319e122c431ae9257
EmotionlessDev <emotionlesscode@gmail.com>
2025-11-24 02:15:21 +1000

    Fix contracts.go

--- internal/domain/team/contracts.go
+++ internal/domain/team/contracts.go
@@ -1,12 +1,12 @@
 package team
 
 type User struct {
-	ID       string `db:"id"`
-	Name     string `db:"name"`
-	TeamName string `db:"team_name"`
-	IsActive bool   `db:"is_active"`
+	ID       string
+	Name     string
+	TeamName string
+	IsActive bool
 }
 
 type Team struct {
-	Name string `db:"team_name"`
+	Name string
 }
